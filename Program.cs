fb0e81b06517cbbae5fd73b7b5210fdd5dd9531a
Stefan Belić <127685759+StefanBelic1@users.noreply.github.com>
2024-01-27 20:54:44 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class CarConfigurator
+{
+    public string Model { get; private set; }
+    private List<Equipment> additionalEquipment = new List<Equipment>();
+
+    public void AddExtra(Equipment package)
+    {
+        additionalEquipment.Add(package);
+    }
+
+    public void Remove(Equipment package)
+    {
+        additionalEquipment.Remove(package);
+    }
+
+    public void Rollback(CarConfiguration configuration)
+    {
+        Model = configuration.GetModel();
+        additionalEquipment.Clear();
+        additionalEquipment.AddRange(configuration.GetPackages());
+    }
+
+    public CarConfiguration Store()
+    {
+        return new CarConfiguration(Model, additionalEquipment);
+    }
+}
+
+
+public class CarConfiguration
+{
+    private string model;
+    private List<Equipment> additionalEquipment;
+
+    public CarConfiguration(string model, List<Equipment> additionalEquipment)
+    {
+        this.model = model;
+        this.additionalEquipment = new List<Equipment>(additionalEquipment);
+    }
+
+    public string GetModel()
+    {
+        return model;
+    }
+
+    public List<Equipment> GetPackages()
+    {
+        return new List<Equipment>(additionalEquipment);
+    }
+
+    public void DisplayConfiguration()
+    {
+        Console.WriteLine($"Car Model: {model}");
+        Console.WriteLine("Additional Equipment:");
+        foreach (var package in additionalEquipment)
+        {
+            Console.WriteLine($"- {package.Name}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+public class ConfigurationManager
+{
+    private List<CarConfiguration> configurations = new List<CarConfiguration>();
+
+    public void AddConfiguration(CarConfiguration configuration)
+    {
+        configurations.Add(configuration);
+    }
+
+    public void DeleteConfiguration(CarConfiguration configuration)
+    {
+        configurations.Remove(configuration);
+    }
+
+    public CarConfiguration GetConfiguration(int index)
+    {
+        return configurations[index];
+    }
+}
+
+
+public class Equipment
+{
+    public string Name { get; set; }
+
+    public Equipment(string name)
+    {
+        Name = name;
+    }
+}
